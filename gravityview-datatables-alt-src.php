1660bbcbdbbaee8c9be90692221179e436fabcf4
Luis Godinho <luis@gomo.pt>
2014-11-12 00:02:48 +0000

    load local scripts

--- gravityview-datatables-alt-src.php
+++ gravityview-datatables-alt-src.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * Plugin Name: GravityView - Use Alternate DataTables Sources
-* Description: Use CDNJS.com sources for the DataTables library
+* Description: Use local files to load DataTables JS
 * Version: 1.0
 * Author: Katz Web Services, Inc.
 * Author URI: https://gravityview.co
@@ -15,29 +15,88 @@ class Replace_GV_DataTables_Scripts {
 
 	function __construct() {
 
+		// datatables
 		add_filter('gravityview_datatables_script_src', array( $this, 'datatables_js') );
 		add_filter('gravityview_datatables_style_src', array( $this, 'datatables_css') );
 
+		// tabletools
 		add_filter('gravityview_dt_tabletools_script_src', array( $this, 'tabletools_js' ) );
 		add_filter('gravityview_dt_tabletools_style_src', array( $this, 'tabletools_css' ) );
 
+		// scroller
+		add_filter('gravityview_dt_scroller_script_src', array( $this, 'scroller_js' ) );
+		add_filter('gravityview_dt_scroller_style_src', array( $this, 'scroller_css' ) );
+
+		// fixedColumns
+		add_filter('gravityview_dt_fixedcolumns_script_src', array( $this, 'fixedcolumns_js' ) );
+		add_filter('gravityview_dt_fixedcolumns_style_src', array( $this, 'fixedcolumns_css' ) );
+
+		// fixedHeader
+		add_filter('gravityview_dt_fixedheader_script_src', array( $this, 'fixedheader_js' ) );
+		add_filter('gravityview_dt_fixedheader_style_src', array( $this, 'fixedheader_css' ) );
+
+		// responsive
+		add_filter('gravityview_dt_responsive_script_src', array( $this, 'responsive_js' ) );
+		add_filter('gravityview_dt_responsive_style_src', array( $this, 'responsive_css' ) );
+
 	}
 
+	// --- datatables --- //
 	function datatables_js() {
-		return '//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.1/js/jquery.dataTables.min.js';
+		return plugins_url( 'js/jquery.dataTables.min.js', __FILE__ );
 	}
 
 	function datatables_css() {
-		return '//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.1/css/jquery.dataTables.min.css';
+		return plugins_url( 'css/jquery.dataTables.min.css', __FILE__ );
 	}
 
+	// --- tabletools --- //
 	function tabletools_js() {
-		return '//cdnjs.cloudflare.com/ajax/libs/datatables-tabletools/2.1.5/js/TableTools.min.js';
+		return plugins_url( 'js/dataTables.tableTools.min.js', __FILE__ );
 	}
 
 	function tabletools_css() {
-		return '//cdnjs.cloudflare.com/ajax/libs/datatables-tabletools/2.1.5/css/TableTools.css';
+		return plugins_url( 'css/dataTables.tableTools.min.css', __FILE__ );
 	}
+
+	// --- scroller --- //
+	function scroller_js() {
+		return plugins_url( 'js/dataTables.scroller.min.js', __FILE__ );
+	}
+
+	function scroller_css() {
+		return plugins_url( 'css/dataTables.scroller.min.css', __FILE__ );
+	}
+
+	// --- fixedColumns --- //
+	function fixedcolumns_js() {
+		return plugins_url( 'js/dataTables.fixedColumns.min.js', __FILE__ );
+	}
+
+	function fixedcolumns_css() {
+		return plugins_url( 'css/dataTables.fixedColumns.min.css', __FILE__ );
+	}
+
+	// --- fixedHeader --- //
+	function fixedheader_js() {
+		return plugins_url( 'js/dataTables.fixedHeader.min.js', __FILE__ );
+	}
+
+	function fixedheader_css() {
+		return plugins_url( 'css/dataTables.fixedHeader.min.css', __FILE__ );
+	}
+
+	// --- responsive --- //
+	function responsive_js() {
+		return plugins_url( 'js/dataTables.responsive.min.js', __FILE__ );
+	}
+
+	function responsive_css() {
+		return plugins_url( 'css/dataTables.responsive.css', __FILE__ );
+	}
+
+
+
 }
 
 add_action( 'init', array('Replace_GV_DataTables_Scripts', 'init' ) );
